8232366a3b29a418f0ffabe4fe427553930be5a9
Michael Fink <michael.fink@live.at>
2025-05-13 12:22:30 +0200

    Add project files.

---
+++ RecipeDatabase/Controllers/HelpController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeDatabase.Controllers
+{
+    public class HelpController : Controller
+    {
+        public IActionResult FAQ(string id)
+        {
+            var idParameterText = " The id parameter was not supplied.";
+            if (!string.IsNullOrEmpty(id))
+            {
+                idParameterText = " The id parameter was supplied.";
+                if (id == "carrot")
+                {
+                    idParameterText = " The id parameter was supplied, and it's value was carrot.";
+                }
+            }
+            return Content("Hello! Here is the FAQ." + idParameterText);
+        }
+    }
+}
